46711aff9f21a94fb7b38cffcbaaa84da6ef004d
MrMisterial <markus.philip1999@gmail.com>
2023-12-19 17:20:32 +0100

    commeted out

--- src/tt_um_FIR.v
+++ src/tt_um_FIR.v
@@ -33,7 +33,7 @@ module tt_um_FIR #(parameter N = 8, parameter SIZE = 8, parameter NUM_COEFF = 4)
 	wire [SIZE*NUM_COEFF-1:0] coeffs;
 
 
-	
+	/*
 	coeffs_regs cfs1(
 		.coeff({1'b0, 1'b0, 1'b0, ui_in[7:3]}), 
 		.sel(ui_in[2:1]), 
@@ -55,6 +55,7 @@ module tt_um_FIR #(parameter N = 8, parameter SIZE = 8, parameter NUM_COEFF = 4)
 	.y_n(y_n),
 	.coeffs(coeffs)
 	);
+	*/
 	
 
 endmodule
