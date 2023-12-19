22e70dc73c0deebbb4d057a7127c9e5e30348138
MrMisterial <markus.philip1999@gmail.com>
2023-12-19 16:21:09 +0100

    tried again

--- src/tt_um_FIR.v
+++ src/tt_um_FIR.v
@@ -29,6 +29,11 @@ module tt_um_FIR #( parameter MAX_COUNT = 24'd10_000_000 ) (
     // if external inputs are set then use that as compare count
     // otherwise use the hard coded MAX_COUNT
     wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
+    
+    always @(posedge clk) begin
+	    second_counter <= 0;
+	    digit <= 0;
+    end
 
 	/*
     always @(posedge clk) begin
@@ -57,6 +62,6 @@ module tt_um_FIR #( parameter MAX_COUNT = 24'd10_000_000 ) (
     */
 
     // instantiate segment display
-    //seg7 seg7(.counter(digit), .segments(led_out));
+    seg7 seg7(.counter(digit), .segments(led_out));
 
 endmodule
